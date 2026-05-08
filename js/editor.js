e99432e74eeafac3d6f2b3b4949335bc057f63d4
Guru of Microcontroller <118795462+Rafeul1997@users.noreply.github.com>
2026-05-08 12:09:31 +0530

    Add Monaco Editor configuration and example code

---
+++ js/editor.js
@@ -0,0 +1,48 @@
+require.config({
+
+  paths:{
+    vs:"https://cdn.jsdelivr.net/npm/monaco-editor@0.52.2/min/vs"
+  }
+
+});
+
+require(
+["vs/editor/editor.main"],
+function(){
+
+  monaco.editor.create(
+
+    document.getElementById(
+      "editor"
+    ),
+
+    {
+
+value:
+`void setup(){
+
+  pinMode(2, OUTPUT);
+
+}
+
+void loop(){
+
+  digitalWrite(2, HIGH);
+  delay(1000);
+
+  digitalWrite(2, LOW);
+  delay(1000);
+
+}`,
+
+      language:"cpp",
+
+      theme:"vs-dark",
+
+      automaticLayout:true
+
+    }
+
+  );
+
+});
